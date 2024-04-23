b830b5d94258dc8fc3b4312a708b915da7509f57
tran viet cuong <zerbythenextscene@gmail.com>
2024-04-23 21:46:40 +0700

    init

---
+++ src/config.ts
@@ -0,0 +1,67 @@
+import type {
+  LicenseConfig,
+  NavBarConfig,
+  ProfileConfig,
+  SiteConfig,
+} from './types/config'
+import { LinkPreset } from './types/config'
+
+export const siteConfig: SiteConfig = {
+  title: 'VCT\'s Story',
+  // subtitle: '',
+  lang: 'vi',
+  themeHue: 250,
+  banner: {
+    enable: false,
+    src: 'assets/images/demo-banner.png',
+  },
+  favicon: [    // Leave this array empty to use the default favicon
+    // {
+    //   src: '/favicon/icon.png',    // Path of the favicon, relative to the /public directory
+    //   theme: 'light',              // (Optional) Either 'light' or 'dark', set only if you have different favicons for light and dark mode
+    //   sizes: '32x32',              // (Optional) Size of the favicon, set only if you have favicons of different sizes
+    // }
+  ]
+}
+
+export const navBarConfig: NavBarConfig = {
+  links: [
+    LinkPreset.Home,
+    LinkPreset.Archive,
+    LinkPreset.About,
+    // {
+    //   name: 'GitHub',
+    //   url: 'https://github.com/saicaca/fuwari',
+    //   external: true,
+    // },
+  ],
+}
+
+export const profileConfig: ProfileConfig = {
+  avatar: 'assets/images/demo-avatar.png',
+  name: 'Viet Cuong Tran',
+  bio: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+  links: [
+    {
+      name: 'Facebook',
+      icon: 'fa6-brands:facebook',
+      url: 'https://www.facebook.com/vct.410/',
+    },
+    // {
+    //   name: 'Steam',
+    //   icon: 'fa6-brands:steam',
+    //   url: 'https://store.steampowered.com',
+    // },
+    // {
+    //   name: 'GitHub',
+    //   icon: 'fa6-brands:github',
+    //   url: 'https://github.com/saicaca/fuwari',
+    // },
+  ],
+}
+
+export const licenseConfig: LicenseConfig = {
+  enable: false,
+  name: 'CC BY-NC-SA 4.0',
+  url: 'https://creativecommons.org/licenses/by-nc-sa/4.0/',
+}
---
+++ src/i18n/languages/vi.ts
@@ -0,0 +1,33 @@
+import Key from '../i18nKey'
+import type { Translation } from '../translation'
+
+export const vi: Translation = {
+  [Key.home]: 'Trang  Chủ',
+  [Key.about]: 'Công việc',
+  [Key.archive]: 'Lưu trữ',
+
+  [Key.tags]: 'Thẻ',
+  [Key.categories]: 'Chủ đề',
+  [Key.recentPosts]: 'Bài viết gần đây',
+
+  [Key.comments]: 'Bình luận',
+
+  [Key.untitled]: 'Chưa có tên',
+  [Key.uncategorized]: 'Chưa có chủ đề',
+  [Key.noTags]: 'Chưa gắn thẻ',
+
+  [Key.wordCount]: 'word',
+  [Key.wordsCount]: 'words',
+  [Key.minuteCount]: 'minute',
+  [Key.minutesCount]: 'minutes',
+  [Key.postCount]: 'Bài đăng',
+  [Key.postsCount]: 'Bài đăng',
+
+  [Key.themeColor]: 'Màu sắc',
+
+  [Key.more]: 'More',
+
+  [Key.author]: 'Tác giả',
+  [Key.publishedAt]: 'Ngày đăng',
+  [Key.license]: 'License',
+}
